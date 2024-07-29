d1b49a4ec0966c75bc48d230d17224e5138a17fa
HP <HP@LAPTOP-0H0T8MO1>
2024-07-29 13:49:40 +0530

    encapsulation

---
+++ JavaBasic/src/com/tnsif/daythree_encap/Employee_excuteor.java
@@ -0,0 +1,22 @@
+package com.tnsif.daythree_encap;
+
+public class Employee_excuteor {
+
+	public static void main(String[] args) {
+		Employee e1 =new Employee();
+		e1.setEid(101);
+		e1.setCity("pondy");
+		e1.setEname("Reventh");
+		e1.setSalary(20000000.0d);
+		System.out.println(e1);
+		Employee e2 =new Employee();
+		e2.setEid(102);
+		e2.setCity("Cudd");
+		e2.setEname("Nivi");
+		e2.setSalary(20000000.0d);
+		System.out.println(e2);
+		
+
+	}
+
+}
